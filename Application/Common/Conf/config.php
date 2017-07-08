540b1ca4e45b3c35aef6121fa77e88eda4913e17
cyx <yx@163.com>
2017-07-08 12:42:33 +0800

    第一次提交

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+   'DB_TYPE' =>  'pdo',     // mysql,mysqli,pdo
+    'DB_DSN'    => 'mysql:host=127.0.0.1;dbname=jdb;charset=utf8',
+    'DB_USER' =>  'root',      // 用户名
+    'DB_PWD' =>  '',          // 密码
+    'DB_PORT' =>  '3306',        // 端口
+    'DB_PREFIX' =>  'p_',    // 数据库表前缀
+    //'DB_HOST' =>  'localhost', // 服务器地址
+    //'DB_NAME' =>  'php39',          // 数据库名
+    //'DB_CHARSET' =>  'utf8',      // 数据库编码默认采用utf8
+    //'DEFAULT_FILTER' => 'trim,htmlspecialchars',
+    
+);
